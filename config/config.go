2b0f4908323879b15a08c4fdc2e1280e51c32934
a2-ito <hi.mound@gmail.com>
2022-06-13 22:36:56 +0900

    update src for golangci-lint

--- config/config.go
+++ config/config.go
@@ -1,25 +1,24 @@
 package config
 
 import (
-	"fmt"
-	"github.com/kelseyhightower/envconfig"
 	"log"
 	"os"
+
+	"github.com/kelseyhightower/envconfig"
+	"go.uber.org/zap"
 )
 
 type Env struct {
 	Port string
 }
 
-// LoadConfig
-func LoadConfig() (e Env) {
-
-	_, result := os.LookupEnv("PORT")
-	if !result {
-		fmt.Println("Not found: PORT")
-		// Todo: 5xx を返却
+func LoadConfig() Env {
+	if _, err := os.LookupEnv("PORT"); !err {
+		zap.S().Error("Not found an env value: PORT")
 	}
+
 	var goenv Env
+
 	err := envconfig.Process("", &goenv)
 	if err != nil {
 		log.Fatal(err.Error())
--- main.go
+++ main.go
@@ -1,22 +1,31 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"go.uber.org/zap"
+
 	"go-echo-template/config"
 	"go-echo-template/helloworld"
-	"net/http"
 )
 
-// ハンドラーを定義
 func hello(c echo.Context) error {
 	return c.String(http.StatusOK, helloworld.Helloworld())
 }
 
 func main() {
-
 	// Echo instance
 	e := echo.New()
+
+	// logger zap
+	logger, _ := zap.NewDevelopment()
+
+	// global
+	zap.ReplaceGlobals(logger)
+
+	logger.Info("Hello zap")
 
 	goenv := config.LoadConfig()
 
@@ -27,5 +36,6 @@ func main() {
 	e.GET("/", hello)
 
 	// Start server
+	logger.Info("http server started on " + goenv.Port)
 	e.Logger.Fatal(e.Start(":" + goenv.Port))
 }
